892dec196c0e93a25b935ed3b1240d4f7cbea236
Vastavikrahul01 <165668531+Vastavikrahul01@users.noreply.github.com>
2024-06-19 11:02:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -102,8 +102,8 @@ VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'publicearn.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '5830f0f64c85e8e34dec20e71c04d34b38104865')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
